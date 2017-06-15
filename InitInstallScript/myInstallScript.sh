bf1ad7872752cbdff55a0642d6d0f2bdb04b5c9d
BhekimpiloNdhela <18998712@sun.ac.za>
2017-06-15 13:36:11 +0200

    innitialised my init installer script

---
+++ InitInstallScript/myInstallScript.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#############################################################################
+# @author  : Bhekimpilo Ndhlela                                             #
+# @since   : Thursday - 15 - 06 - 2017                                      #
+# @version : v1.0                                                           #
+#############################################################################
+
+sudo apt-get update
+
+
+#restart machine after the installation of these components.
+reboot
